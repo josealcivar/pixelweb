464a62433de6129323c7378abc7d5d9dd5c61fd6
josealcivar <jose_alcivargarcia@hotmail.com>
2018-10-18 19:52:04 -0500

    correciones de errores

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 		// 		target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 		// 		if (target.length) {
 					$('html, body').animate({
-						scrollTop: target.offset().top
+						scrollTop: $( $(this).attr('href') ).target.offset().top
 					}, 1000);
 					return false;
 		// 		}
